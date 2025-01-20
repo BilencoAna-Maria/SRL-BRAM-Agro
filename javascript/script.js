9c1be9158036f065c8aa01635464ae1726e8cb93
Bilenco Ana-Maria <anmariaudiobook@gmail.com>
2025-01-20 10:37:21 +0200

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,24 @@
+  // Inside your Javascript file
+  function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+function mobileMenu() {
+  var x = document.getElementsByTagName("navbar")[0];
+  if (x.className === "") {
+      x.className += "mobile";
+  } else {
+      x.className = "";
+  }
+}
